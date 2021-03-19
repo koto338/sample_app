f444d87fef1e4792499184efd94b056e55864d8b
EC2 Default User <monmo4909@gmail.com>
2021-03-20 01:38:11 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user 
+      params[:session][:remember_me] == '1'? remember(user) : forget(user)
+      # remember user　sessionの中身が空でなくなってしまっていたので、テストが失敗
       redirect_to user
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
@@ -16,7 +18,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
